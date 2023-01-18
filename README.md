a3419404e0e8728ee7a45acdf8dd1804476f69ed
KSVarlamov <varlamov.ks@mail.ru>
2023-01-18 11:21:08 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Домашнее задание 3. Задача 1. Генерация никнеймов
+
+Решение задачи [отсюда](https://github.com/netology-code/jd-homeworks/tree/video/volatile/task1)
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,113 @@
+package org.example;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+
+    private static AtomicInteger counter3 = new AtomicInteger(0);
+    private static AtomicInteger counter4 = new AtomicInteger(0);
+    private static AtomicInteger counter5 = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        Random random = new Random();
+        String[] texts = new String[100_000];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("abc", 3 + random.nextInt(3));
+        }
+        Thread palindrome = new Thread(() -> {
+            for (String s : texts) {
+                boolean isPalindrome = true;
+                int length = s.length();
+                for (int i = 0; i < (length / 2); i++) {
+                    if (s.charAt(i) != s.charAt(length - i - 1)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                if (isPalindrome) {
+                    switch (length) {
+                        case 3:
+                            counter3.incrementAndGet();
+                            break;
+                        case 4:
+                            counter4.incrementAndGet();
+                            break;
+                        case 5:
+                            counter5.incrementAndGet();
+                    }
+                }
+            }
+        });
+        Thread monoLetter = new Thread(() -> {
+            for (String s : texts) {
+                boolean isMonoLetter = true;
+                int length = s.length();
+                char first = s.charAt(0);
+                for (int i = 1; i < (length); i++) {
+                    if (s.charAt(i) != first) {
+                        isMonoLetter = false;
+                        break;
+                    }
+                }
+                if (isMonoLetter) {
+                    switch (length) {
+                        case 3:
+                            counter3.incrementAndGet();
+                            break;
+                        case 4:
+                            counter4.incrementAndGet();
+                            break;
+                        case 5:
+                            counter5.incrementAndGet();
+                    }
+                }
+            }
+        });
+        Thread sortedStr = new Thread(() -> {
+            for (String s : texts) {
+                boolean isSorted = true;
+                int length = s.length();
+                char current = s.charAt(0);
+                for (int i = 1; i < (length); i++) {
+                    if (s.charAt(i) < current) {
+                        isSorted = false;
+                        break;
+                    } else {
+                        current = s.charAt(i);
+                    }
+                }
+                if (isSorted) {
+                    switch (length) {
+                        case 3:
+                            counter3.incrementAndGet();
+                            break;
+                        case 4:
+                            counter4.incrementAndGet();
+                            break;
+                        case 5:
+                            counter5.incrementAndGet();
+                    }
+                }
+            }
+        });
+
+        palindrome.start();
+        monoLetter.start();
+        sortedStr.start();
+        palindrome.join();
+        monoLetter.join();
+        sortedStr.join();
+        System.out.printf("Красивых слов с длиной %s: %s шт\n", 3, counter3);
+        System.out.printf("Красивых слов с длиной %s: %s шт\n", 4, counter4);
+        System.out.printf("Красивых слов с длиной %s: %s шт\n", 5, counter5);
+    }
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
